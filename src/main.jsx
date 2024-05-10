a820e3258f7470db7e9af3fc6fc34d321f53c26c
fihry <fihrylara@gmail.com>
2024-05-10 23:26:18 +0100

    edit basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename="/HVM">
+  <BrowserRouter basename="/">
     <App/>
   </BrowserRouter>
 )
